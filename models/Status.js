5be10186edf7108f1d57910490f917310b4d7b4e
zero <826474nilwan@gmail.com>
2025-07-29 00:27:32 +0530

    cleared everything

--- models/Status.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const statusSchema = new mongoose.Schema({
-  status: String,
-  date: Date,
-});
-
-module.exports = mongoose.model("Status", statusSchema);
--- models/Update.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const updateSchema = new mongoose.Schema({
-  text: String,
-  date: String,
-});
-
-module.exports = mongoose.model("Update", updateSchema);
